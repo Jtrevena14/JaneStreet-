7fc7feb7b6819c69760a9d17c3cbda4b6a240399
andrewely8 <andrewely08@gmail.com>
2026-05-15 21:56:57 -0400

    drawing visual

--- puzzle.py
+++ puzzle.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+from matplotlib.patches import Arc
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+
+archs = ["none","topleft","topright","bottomleft","bottomright"]
 
 board = [
 	[0,0,21,0,0,0,0,0,0],
@@ -40,6 +43,25 @@ for i in range(len(board)):
 print(final_board[0])
 
 
+
+
+def drawArch(arch,i,j):
+
+	if arch == "topleft":
+		t1,t2 = 90,180
+		x,y = (j+1)*16,(i+1)*16
+	elif arch == "topright":
+		t1,t2 = 180,270
+		x,y = (j)*16,(i+1)*16 
+	elif arch == "bottomleft":
+		t1,t2 = 0,90
+		x,y = (j+1)*16,(i)*16 
+	elif arch == "bottomright":
+		t1,t2 = 270,360
+		x,y = (j)*16,(i)*16
+
+	return patches.Arc((x,y),32,32,angle=90,theta1=t1,theta2=t2)
+
 def visual(arr,num_rows=9,num_cols=9,cell_size=16,cell_color='white',edge_color='black'):
 	ax = plt.gca()
 
@@ -48,6 +70,18 @@ def visual(arr,num_rows=9,num_cols=9,cell_size=16,cell_color='white',edge_color=
 
 			rect = plt.Rectangle([j*16, i*16], cell_size, cell_size,facecolor=arr[i][j]['color'],edgecolor=edge_color)
 			ax.add_patch(rect)
+			if not arr[i][j]["cv"] == "none":
+				arch = drawArch(arr[i][j]["cv"],j,i)
+				ax.add_patch(arch)
+			# e1 = patches.Arc((1*16, 1*16),32,32,angle=90,theta1=90,theta2=180)
+			# e2 = patches.Arc((0*16, 1*16),32,32,angle=90,theta1=180,theta2=270)
+			# e3 = patches.Arc((1*16, 1*16),32,32,angle=90,theta1=270,theta2=360)
+			# e4 = patches.Arc((1*16, 1*16),32,32,angle=90,theta1=0,theta2=90)
+			
+			# ax.add_patch(e1)
+			# ax.add_patch(e2)
+			# ax.add_patch(e3)
+			# ax.add_patch(e4)
 			plt.text(j*16+8,i*16+8, arr[i][j]['val'],fontsize="12",ha="center")
 
 	ax.axis('off')
